05891232fb48884199ea059e04771769c2db674a
prateikdubey <prateikdubey1997@gmail.com>
2024-08-07 17:29:08 +0530

    Storybook references, Welcome UI text color

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -6,7 +6,7 @@ export function Welcome() {
     <>
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'blue', to: 'cyan' }}>
           Mantine
         </Text>
       </Title>
