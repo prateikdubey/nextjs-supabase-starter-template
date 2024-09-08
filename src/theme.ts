c367197742ee1147f2ef902f592717cd5e73de93
prateikdubey <prateikdubey1997@gmail.com>
2024-09-09 04:09:07 +0530

    Theme Changes

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,32 @@
 'use client';
 
-import { createTheme } from '@mantine/core';
+import { createTheme, MantineColorsTuple, MantineRadiusValues } from '@mantine/core';
+
+const lightBlue: MantineColorsTuple = [
+  '#e0fbff',
+  '#cbf2ff',
+  '#9ae2ff',
+  '#64d2ff',
+  '#3cc5fe',
+  '#23bcfe',
+  '#09b8ff',
+  '#00a1e4',
+  '#0090cd',
+  '#007cb5'
+];
+
+const radius: MantineRadiusValues = {
+  xs: "12px",
+  sm: "14px",
+  md: "16px",
+  lg: "18px",
+  xl: "20px",
+}
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: 'lightBlue',
+  colors: {
+    lightBlue,
+  },
+  radius
 });
